58c2075f89cc770171a8d42039ff608c9e3348ee
KhorinVitaly <khorin.v@gmail.com>
2017-09-08 12:59:32 +0600

    fix mistakes
    use 'isinstance' and chek price if it is string

--- format_price.py
+++ format_price.py
@@ -1,13 +1,21 @@
 def format_price(price):
-    if not type(price) == int and not type(price) == float:
+    if not isinstance(price, (float, int, str)):
         return None
-    if type(price) == float and price % 1 == 0:
+
+    if isinstance(price, str):
+        try:
+            price = float(price)
+        except ValueError:
+            return None
+
+    if isinstance(price, float) and price % 1 == 0:
         price = int(price)
+
     return '{:,}'.format(price).replace(',', ' ')
 
 if __name__ == '__main__':
-    try:
-        price = float(input('input price to format: '))
+    price = format_price(input('input price to format: '))
+    if price:
         print('formatted price: ' + format_price(price))
-    except ValueError:
+    else:
         print('incorrect input')
